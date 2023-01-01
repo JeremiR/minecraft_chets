12eb5a592727414051d6cb01ca20732264a2ab94
JeremiR <jeremi.r2012@gmail.com>
2023-01-01 13:54:55 +0100

    add some text

--- jquery/MyJavaskript.js
+++ jquery/MyJavaskript.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
         $(this).replaceWith('<h3>' + $ (this).text() + '</h3>');
     });
 });
-var rquiredFlag = ' * ';
+var requiredFlag = ' * ';
 var codicinalFlag = ' ** ';
-
+var requiredKey = $('input.required : first') .next('span') .text();
+var codicinalKey
